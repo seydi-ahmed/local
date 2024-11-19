11a2c3883ee70b937ec5a6df9d0f9c53ed7e1f11
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-11-19 16:22:07 +0000

    bleme de virtualisation

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.email "seydiahmedelcheikh@gmail.com"
 git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
-git commit -m "01lOCAL"
+git commit -m "bleme de virtualisation"
 git push
