17e8c20bd8cee7fbba55ad82ca064a701c29283c
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2025-01-21 14:50:56 +0000

    simple shell

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.email "seydiahmedelcheikh@gmail.com"
 git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
-git commit -m "netcat server"
+git commit -m "simple shell"
 git push
